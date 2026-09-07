06f439f83658dadb19019eb5c3c18b55c4fc43bb
Brian-lo <maplecafejapan@gmail.com>
2026-09-07 18:48:57 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fuji-recipe-v23";
+const CACHE = "fuji-recipe-v24";
 const PRECACHE = ["./","index.html","polaroid.html","manifest.webmanifest","icon-192.png","icon-512.png"];
 
 self.addEventListener("install", event => {
